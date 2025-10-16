a0d26edbb84f7ed1cf1441867830e7c8929245fc
Rohith Reddy GK <gkrohithreddy@gmail.com>
2025-10-16 20:54:51 +0530

    Updation done

--- my-portfolio/vite.config.js
+++ my-portfolio/vite.config.js
@@ -3,10 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: 'es2019', // make JS compatible with react-snap
+  },
   optimizeDeps: {
     include: ["react-helmet-async"]
   },
   resolve: {
     dedupe: ["react", "react-dom"]
   }
-})
+});
